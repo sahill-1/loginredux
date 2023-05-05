31944ec648392a7466d0e3d335b0aba99ccd5c45
sahill-1 <sv260397@gmail.com>
2023-05-05 17:57:19 +0530

    login react redux

---
+++ src/Components/Login.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import "./Login.css";
+const Login = () => {
+    const [email, setEmail] = useState(" ");
+    const [password, setPassword] = useState(" ");
+  return (
+    <div className='login'>
+      <form className='login__form'>
+        <h1>Login Here</h1>
+        <label>Email</label>
+        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label>Password</label>
+        <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button type='submit' className='submit__btn'>LOGIN</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Components/Logout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Logout.css";
+
+const Logout = () => {
+  return (
+    <div>
+      <h1 >Welcome <span className='user__name'>Shangai</span>
+      <button className='logout__button'>Logout</button>
+      </h1>
+    </div>
+  )
+}
+
+export default Logout
