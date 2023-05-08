44e9695134e8d816f8d395137e1c6352394f98e7
sahill-1 <sv260397@gmail.com>
2023-05-08 15:22:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Login from './Components/Login';
+import {useSelector} from "react-redux";
+import { selectUser } from './features/userSlice';
+import Logout from './Components/Logout';
 
 function App() {
+  const user = useSelector(selectUser)
   return (
-    <>
-    <Login/>
-    </>
+    <div>
+    {user ? <Logout/> : <Login/>}
+    </div>
   );
 }
 
--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import "./Logout.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../features/userSlice';
 
 const Logout = () => {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+
+    dispatch(logout())
+  }
   return (
-    <div>
-      <h1 >Welcome <span className='user__name'>Shangai</span>
-      <button className='logout__button'>Logout</button>
-      </h1>
+    <div className='logout'>
+      <h1 >Welcome <span className='user__name'>{user.email}</span></h1>
+      <button className='logout__button' onClick={ (e) => handleLogout(e)}>Logout</button>
     </div>
   )
 }
