e9abb7aadadf28203f9ee6df3cc8e9b80b86dd33
sahill-1 <sv260397@gmail.com>
2023-05-08 12:28:16 +0530

    data is getting stored in Main Store

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,17 +1,38 @@
 import React, { useState } from 'react'
 import "./Login.css";
+import { useDispatch } from 'react-redux';
+import { login } from '../features/userSlice';
+
 const Login = () => {
-    const [email, setEmail] = useState(" ");
-    const [password, setPassword] = useState(" ");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      login({
+    
+      email: email,
+      password: password,
+      loggedIn: true,
+    })
+    )
+  }
+
   return (
     <div className='login'>
-      <form className='login__form'>
+      <form className='login__form' onSubmit={(e) => handleSubmit(e)}>
         <h1>Login Here</h1>
         <label>Email</label>
         <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
         <label>Password</label>
-        <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type='submit' className='submit__btn'>LOGIN</button>
+
       </form>
     </div>
   )
